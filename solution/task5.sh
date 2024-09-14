c39fbc17b979d30e7c26b4cdedfb7b79522f5ad9
Yegor Yershov <yegor6741@gmail.com>
2024-09-14 14:37:10 +0700

    task5 fixes v2

--- solution/task5.sh
+++ solution/task5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # cat data/students.csv | sed -E 's/(\w+)\s+(\w+),([0-9]*)/\1 (\3)/'
-python3 -c "[print(f'{line.split()[1].split(',')[0]} {line.split()[0]} ({line.split(',')[-1]})') for line in open('data/students.csv').read().split('\n')[:-1:]]" > data/students.txt
+python3 -c "[print(f\"{line.split()[1].split(',')[0]} {line.split()[0]} ({line.split(',')[-1]})\") for line in open('data/students.csv').read().split('\n')[:-1:]]" > data/students.txt
