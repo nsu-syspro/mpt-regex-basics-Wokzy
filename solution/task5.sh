f6c10dada9ef48eda09663713219462bb5fdce8e
Yegor Yershov <yegor6741@gmail.com>
2024-09-15 14:47:21 +0700

    update solution

--- solution/task5.sh
+++ solution/task5.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# cat data/students.csv | sed -E 's/(\w+)\s+(\w+),([0-9]*)/\1 (\3)/'
-python3 -c "[print(f\"{line.split()[1].split(',')[0]} {line.split()[0]} ({line.split(',')[-1]})\") for line in open('data/students.csv').read().split('\n')[:-1:]]" > data/students.txt
+cat data/students.csv | sed -E 's/^(\S+) (\S+).*,([0-9]*)/\2 \1 (\3)/'
